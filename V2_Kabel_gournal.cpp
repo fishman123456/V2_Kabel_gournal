dc31b656515218adecde40c970f59fc8c3ceed2a
denis <106389581+fishman123456@users.noreply.github.com>
2022-09-06 16:41:21 +0700

    рабочая версия
    Соединяем строки для вставки блоков

--- V2_Kabel_gournal.cpp
+++ V2_Kabel_gournal.cpp
@@ -7,6 +7,12 @@
 using namespace std;
 
 int main() {
+	string lisp_1;
+	string lisp_2;
+	string lisp_3;
+	string string_sum1;
+	string string_sum2;
+	string string_sum3;
 	//setlocale(LC_ALL, "RUS");
 	//system("chcp 1251"); // настраиваем кодировку консоли
 	ifstream file;            // создать поточный объект file
@@ -18,10 +24,24 @@ int main() {
 		cout << str << endl;     // вывод прочитанной строки на экран
 	cin.sync(); cin.get();
 	ifstream input("test.txt"); //Открываешь первый файл для чтения
-	string string; //Строка-буфер
+	string string_buf; //Строка-буфер
 	ofstream output("output.txt"); //Открываешь второй файл для записи
-	while (getline(input, string, ' '))  //Считываешь из файла строку (пока не EOF)End Of File
-		output << string << endl;  //И записываешь эту строку в выходной файл
+	while (getline(input, string_buf))  //Считываешь из файла строку (пока не EOF)End Of File
+	{
+		lisp_1 = "(command \"_. - layer\" \"_m\" \""; // строка из лиспа для автокада - начало
+		lisp_1.append (string_buf); // складываем строку из файла и строку lisp1
+		if (getline(input, string_buf, ' '))
+		{
+			lisp_2 = "\" \"\") (vla - InsertBlock obj(vlax - 3D - point '(4000	107376 0))  \"";// строка из лиспа для автокада - середина
+				lisp_1.append(lisp_2); // складываем строку из файла и строку lisp2
+		}
+		lisp_1.append(string_buf);
+		lisp_3 = "\" 1 1 1 0)";// строка из лиспа для автокада - конец
+		lisp_1.append(lisp_3);
+		//string_sum3 = lisp_3 + string_buf; // складываем строку из файла и строку lisp3
+
+		output << lisp_1 << endl;  //И записываешь эту строку в выходной файл
+	}
 	system("pause");
 	return 0;
 }
