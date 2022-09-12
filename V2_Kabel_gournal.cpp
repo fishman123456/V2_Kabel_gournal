326c909d2d68f73b6d5c84d5a94945995e6de918
denis <106389581+fishman123456@users.noreply.github.com>
2022-09-12 12:36:39 +0700

    строки которые надо добавить в файл
    и как-то их вывести

--- V2_Kabel_gournal.cpp
+++ V2_Kabel_gournal.cpp
@@ -2,6 +2,26 @@
 //http://blog.kislenko.net/show.php?id=1402
 // в качестве разделителя используем знаки < > имя слоя и имя блока на одной строке 
 // 7.HA-S1<siren>
+// Надо добавить в программу сборку строк это начо файла
+//  (defun C : F_Blockinsert(/ x1 x2 x3)
+//	(setq actdoc(vla - get - ActiveDocument(vlax - get - acad - object)))
+//	(setq obj(vla - get - ModelSpace actdoc))
+//	(setq x1 1); начальное значение списка равно нулю, потом его увеличиваем на еденицу и получаем список
+//	(setq x2 1); на сколько будем увеличивать порядковый номер элемента списка
+//	; Запоминаем текущее состояние режимов ORTHO, SNAP И OSNAP
+//	(setq	old_ortho(getvar "ORTHOMODE")
+//		old_snap(getvar "SNAPMODE")
+//		old_osnap(getvar "OSMODE")
+//	)
+//	; отключаем режимы ORTHO, SNAP И OSNAP
+//	(princ "\n Input : F_Blockinsert "); формирование файла происходит с помощью С++, Спасибо обучению в Академми ШАГ, ныне ТОР
+//	(setvar "ORTHOMODE" 0); и лично Бакун Александр Владимтрович
+//	(setvar "SNAPMODE" 0)
+//	(setvar "OSMODE" 0)
+//-----------------
+// Это конец файла
+//   (alert "Закончили")
+//)
 #include<Windows.h>
 #include <iostream>
 #include <fstream>
